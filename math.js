d3fd11f3a5a0823162652a04c3f1e8e0cac4a5c0
Dov Jacobson <Dov@GamesThatWork.com>
2021-04-12 13:38:20 -0400

    Replace Chinese w nested, classified Fake Equation

--- math.js
+++ math.js
@@ -27,11 +27,12 @@ export const newMath = cnfg => {
         parent.style.display="block";
         return self;
         },
-      pulse: ()=>  self.construct( eq.head + eq.pulse                                                             ),
-      wave:  ()=>  self.construct( eq.head             + eq.cosine                                      ),
-      decay: ()=>  self.construct( eq.head + '<mfrac>' + eq.cosine + eq.decay + '</mfrac>'              ),
-      prop:  ()=>  self.construct( eq.head +                                                 eq.cuberoot),
-      full:  ()=>  self.construct( eq.head + '<mfrac>' + eq.cosine + eq.decay + '</mfrac>' + eq.cuberoot),
+      power: ()=>  self.construct( eq.head + eq.power               + eq.tail ),
+      pulse: ()=>  self.construct( eq.head + eq.pulse               + eq.tail ),
+      wave:  ()=>  self.construct( eq.head + eq.cosine              + eq.tail ),
+      decay: ()=>  self.construct( eq.head + eq.decay               + eq.tail ),
+      prop:  ()=>  self.construct( eq.head + eq.propa            + eq.tail ),
+      full:  ()=>  self.construct( eq.head + eq.decay + eq.propa + eq.tail ),
 
 
       }
@@ -40,147 +41,219 @@ export const newMath = cnfg => {
 
 
 
+const e = part=>eq[part];
+
+const eq={}
+
+eq.hi = `<mrow color="yellow">`;
+eq.ih = `</mrow>`;
 
 
-const eq={
 
-head:`
-<math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+
+eq.head= 
+`<math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
   <mi>p</mi>
-  <mo>=</mo>`,
-pulse: `  <mfrac>
-<mrow>
-  <mn>0.9575</mn>
-  <mi>t</mi>
-</mrow>
-<mrow>
-  <msup>
-    <mi>R</mi>
-    <mfrac>
-      <mn>3</mn>  
-      <mn>4</mn>  
-    </mfrac>
-  </msup>
-  <msup>
-    <mi>w</mi>
-    <mfrac>
-      <mn>1</mn>  
-      <mn>12</mn>  
-    </mfrac>
-  </msup>
-</mrow>
-</mfrac>`,
-cosine:`
-  <mrow>
-    <mn>0.1838</mn>
-    <mo>cos</mo>
-    <mfenced  separators="" >    
-      <mfrac>
-        <mrow>
-          <mn>0.9575</mn>
-          <mi>t</mi>
-        </mrow>
-        <mrow>
-          <msup>
-            <mi>R</mi>
-            <mfrac>
-              <mn>3</mn>  
-              <mn>4</mn>  
-            </mfrac>
-          </msup>
-          <msup>
-            <mi>w</mi>
-            <mfrac>
-              <mn>1</mn>  
-              <mn>12</mn>  
-            </mfrac>
-          </msup>
-        </mrow>
-      </mfrac>
-      <mo>+</mo>
-      <mfrac>
-        <mi>π</mi>
-        <mn>4</mn>
-      </mfrac>
-    </mfenced>
-  </mrow>`,
+  <mo>=</mo>`;
+//<mo>&prop;</mo>`;
 
-  decay:`
-  <mrow>
-    <msup>
-      <mfenced  separators="" >
-       <mfrac>
-        <mrow>
-          <mn>1.915</mn>
-          <msup>
-            <mi>w</mi>
-            <mfrac>
-              <mn>1</mn>
-              <mn>4</mn>
-            </mfrac>
-          </msup>
-          </mrow>
-        <msup>
-          <mi>R</mi>
+eq.power= 
+`<mi class="power">&omega;</mi>`;
+eq.pulse=
+ `
+ <mrow>
+ ${eq.power}
+ <mo>&sdot;</mo>
+ <mrow class="pulse">
+ <mi>H</mi>
+ <mfenced  separators="" >    
+ <mi>S</mi>
+ <mfrac>
+      <mrow>
+        <mi>t</mi>
+      </mrow>
+      <mrow>
+        <mi>R</mi>
+      </mrow>
+    </mfrac>
+   </mfenced>    
+  </mrow>
+</mrow>
+   `;
+eq.cosine=`
+<mrow>  
+${eq.pulse}
+  <mrow class="cosine">
+   <mo>sin</mo>
+    <mfenced  separators="" >    
+    <mfrac>
+    <mrow>
+      <mi>t</mi>
+    </mrow>
+    <mrow>
+      <mi>R</mi>
+    </mrow>
+  </mfrac>
+ </mfenced>
+ </mrow>
+</mrow>
+ `;
+
+eq.decay=`
+<mfrac>
+  ${eq.cosine}
+       
+  <mrow class="decay">
+    <mn>D</mn>
+    <mfenced  separators="" >    
+      <mn>t</mn>
+    </mfenced>
+  </mrow>
+</mfrac>
+  `;
+
+eq.propa=
+` <mfenced separators=""  class="propa" >
+    <mfrac>
+      <mrow>
+        <mn>1</mn>
+      </mrow>
+      <mrow>
+       <msup>  
+         <mi>R</mi>
+         <mn>3</mn>
+       </msup>
+      </mrow>
+    </mfrac>
+ </mfenced>
+ `;
+
+eq.tail=`</math>`;
+
+
+eq.full= eq.head + eq.decay +eq.propa +eq.tail;
+
+
+
+const eqML= eq.head + '<mfrac>' + eq.cosine + eq.decay + '</mfrac>' + eq.propa;
+
+
+/*
+
+   
+    
+    
+    const eq={
+    
+    head:`
+    <math xmlns="http://www.w3.org/1998/Math/MathML" display="block">
+      <mi>p</mi>
+      <mo>=</mo>`,
+    pulse: `  <mfrac>
+    <mrow>
+      <mn>0.9575</mn>
+      <mi>t</mi>
+    </mrow>
+    <mrow>
+      <msup>
+        <mi>R</mi>
+        <mfrac>
+          <mn>3</mn>  
+          <mn>4</mn>  
+        </mfrac>
+      </msup>
+      <msup>
+        <mi>w</mi>
+        <mfrac>
+          <mn>1</mn>  
+          <mn>12</mn>  
+        </mfrac>
+      </msup>
+    </mrow>
+    </mfrac>`,
+    cosine:`
+      <mrow>
+        <mn>0.1838</mn>
+        <mo>cos</mo>
+        <mfenced  separators="" >    
           <mfrac>
-            <mn>7</mn>
+            <mrow>
+              <mn>0.9575</mn>
+              <mi>t</mi>
+            </mrow>
+            <mrow>
+              <msup>
+                <mi>R</mi>
+                <mfrac>
+                  <mn>3</mn>  
+                  <mn>4</mn>  
+                </mfrac>
+              </msup>
+              <msup>
+                <mi>w</mi>
+                <mfrac>
+                  <mn>1</mn>  
+                  <mn>12</mn>  
+                </mfrac>
+              </msup>
+            </mrow>
+          </mfrac>
+          <mo>+</mo>
+          <mfrac>
+            <mi>π</mi>
             <mn>4</mn>
           </mfrac>
-        </msup>
-     </mfrac> 
-  
-  
-        <mi>t</mi>
-      </mfenced>
-      <mn>1.4</mn>
-    </msup>
-  <mo>+</mo>
-  <mn>0.13</mn>
-  
-  
-  </mrow>`,
-
-
-cuberoot:
-`<mo>.</mo>
-
-  <mfenced  separators="" >
-    <mfrac>
-      <mrow>
-        <msub>
-          <mi>S</mi>
-          <mn>1</mn>
-        </msub>
-      </mrow>
-      <mrow>
-        <mfrac>
-          <mrow>  
-            <mi>R</mi>
-          </mrow>
-          <mrow> 
-            <mroot>
-              <mi>w</mi>
-              <mn>3</mn>
-            </mroot>
-          </mrow>
-        </mfrac>
-      </mrow>
-    </mfrac>
-      
-  
-
-    <mo >+</mo>
+        </mfenced>
+      </mrow>`,
     
-    <mfrac>
-      <mrow>
-        <msub>
-          <mi>S</mi>
-          <mn>2</mn>
-        </msub>
-      </mrow>
+      decay:`
       <mrow>
         <msup>
           <mfenced  separators="" >
+           <mfrac>
+            <mrow>
+              <mn>1.915</mn>
+              <msup>
+                <mi>w</mi>
+                <mfrac>
+                  <mn>1</mn>
+                  <mn>4</mn>
+                </mfrac>
+              </msup>
+              </mrow>
+            <msup>
+              <mi>R</mi>
+              <mfrac>
+                <mn>7</mn>
+                <mn>4</mn>
+              </mfrac>
+            </msup>
+         </mfrac> 
+      
+      
+            <mi>t</mi>
+          </mfenced>
+          <mn>1.4</mn>
+        </msup>
+      <mo>+</mo>
+      <mn>0.13</mn>
+      
+      
+      </mrow>`,
+    
+    
+    propa:
+    `<mo>.</mo>
+    
+      <mfenced  separators="" >
+        <mfrac>
+          <mrow>
+            <msub>
+              <mi>S</mi>
+              <mn>1</mn>
+            </msub>
+          </mrow>
+          <mrow>
             <mfrac>
               <mrow>  
                 <mi>R</mi>
@@ -192,44 +265,73 @@ cuberoot:
                 </mroot>
               </mrow>
             </mfrac>
-          </mfenced> 
-          <mn>2</mn>
-        </msup>
-      </mrow>
-    </mfrac>
-
-  <mo>+</mo>
-
-  <mfrac>
-    <mrow>
-      <msub>
-        <mi>S</mi>
-        <mn>3</mn>
-      </msub>
-    </mrow>
-    <mrow>
-      <msup>
-        <mfenced  separators="" >
-          <mfrac>
-            <mrow>  
-              <mi>R</mi>
-            </mrow>
-            <mrow> 
-              <mroot>
-                <mi>w</mi>
-                <mn>3</mn>
-              </mroot>
-            </mrow>
-          </mfrac>
-        </mfenced> 
-        <mn>3</mn>
-      </msup>
-    </mrow>
-  </mfrac>
- </mfenced>          
-</math>
-`};
-
-
-const eqML= eq.head + '<mfrac>' + eq.cosine + eq.decay + '</mfrac>' + eq.cuberoot;
-
+          </mrow>
+        </mfrac>
+          
+      
+    
+        <mo >+</mo>
+        
+        <mfrac>
+          <mrow>
+            <msub>
+              <mi>S</mi>
+              <mn>2</mn>
+            </msub>
+          </mrow>
+          <mrow>
+            <msup>
+              <mfenced  separators="" >
+                <mfrac>
+                  <mrow>  
+                    <mi>R</mi>
+                  </mrow>
+                  <mrow> 
+                    <mroot>
+                      <mi>w</mi>
+                      <mn>3</mn>
+                    </mroot>
+                  </mrow>
+                </mfrac>
+              </mfenced> 
+              <mn>2</mn>
+            </msup>
+          </mrow>
+        </mfrac>
+    
+      <mo>+</mo>
+    
+      <mfrac>
+        <mrow>
+          <msub>
+            <mi>S</mi>
+            <mn>3</mn>
+          </msub>
+        </mrow>
+        <mrow>
+          <msup>
+            <mfenced  separators="" >
+              <mfrac>
+                <mrow>  
+                  <mi>R</mi>
+                </mrow>
+                <mrow> 
+                  <mroot>
+                    <mi>w</mi>
+                    <mn>3</mn>
+                  </mroot>
+                </mrow>
+              </mfrac>
+            </mfenced> 
+            <mn>3</mn>
+          </msup>
+        </mrow>
+      </mfrac>
+     </mfenced>          
+    </math>
+    `};
+   
+    
+    const eqML= eq.head + '<mfrac>' + eq.cosine + eq.decay + '</mfrac>' + eq.propa;
+    
+*/
