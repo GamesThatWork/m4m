e87bc7e5ee150b766a8af14463ce3cc68390b621
Dov Jacobson <Dov@GamesThatWork.com>
2021-07-05 03:46:44 -0400

    begib to clean up and centralize  the signal bus

---
+++ signal.js
@@ -0,0 +1,17 @@
+
+export const newSignal = () => {
+	const self ={
+		bus:	document.querySelector("#bus"),
+		list:	[],
+		fire: 	(event, data   ) => 	self.bus.dispatchEvent( new CustomEvent( event, { details:data } ) ),
+		on:   	(event, handler) =>{ 	self.bus.addEventListener( event, handler );
+										return self.list.push( {event, handler } );
+										},
+		off:   	i				 =>{ 	let o = self.list[ i ];
+										self.bus.removeEventListener( o.event, o,handler );
+										},
+		clear:  ()=>   self.bus.parentNode.replaceChild( self.bus.cloneNode(true), signal.bus),
+		}
+
+	return self;		
+}
