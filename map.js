6eca241cef3eb36c484c424edd79abad42c87420
Dov Jacobson <Dov@GamesThatWork.com>
2021-07-06 09:33:09 -0400

    maps moved back into dom

--- map.js
+++ map.js
@@ -1,19 +1,43 @@
-export const newMap = cfg => {
 
-	var g=false; 
+const url= { 	
+	server: "./assets/map/",
+	range :  "eglinred.jpg",
+	test  :  "bigmap.svg",
+	};
+const dom ={
+	parent: document.querySelector("#map"), 
+	}
+const selfs=[];
+ 
 
+export const newMap = (name,cfg={}) => {
+
+	console.log("map", name );
+	if( selfs[ name ] )	return selfs[name];
+		
+	
     const self={
-		get g() { return g },
+		get dom() { return dom[ name ] },
 		construct: ()=>{
-			g = new PIXI.Sprite.from( '/assets/map/eglinred.jpg');
-			cfg.parent.addChild( g )
-			return self;
+			dom[ name ] = document.createElement("div");
+			dom[ name ].id= name;
+			dom[ name ].classList.add("map");
+			dom.parent.appendChild( dom[ name ] );
+			let imgfile= cfg.img || name;
+			if( url[ imgfile ].includes(".svg") )
+				fetch(   url.server + url[ imgfile ] )
+					.then(response => response.text())
+					.then(  svg => dom[ name ].innerHTML = svg)
+					.catch( err => dom.root.innerHTML=err );
+			else	dom[ name ].style.backgroundImage=
+						`url("${ url.server + url[ imgfile ] }")`;
+			return selfs[ name ]=self;
 			},
-		hide: ()	=>{	g.visible=false;			return self;},
-		show: ()	=>{	g.visible=true; 			return self;},
-		move: (x,y)	=>{	g.position.set(x,y);		return self;},
-		size: (x,y)	=>{	g.width=x; g.height=y;		return self;},
-		destruct: ()=>g.destroy(),
+
+		img:   i =>{	/*unsupported as command*/   return self;},
+		move:  m =>	{ dom[ name ].style.transform=`translate(${m[0]}px,${m[1]}px) scale(${m[2]})`; return self;	  },
+		show:  ()=> { dom[ name ].style.display="block";                                           return self;   },
+		hide:  ()=> { dom[ name ].style.display="none"; console.log("Hide ", name );               return self;   },
         }
-    return self.construct();
+    return selfs[ name ]=self.construct();
     }
