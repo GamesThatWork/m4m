95fc213300763368fa6bd19608a607d6c7d8bb7c
Dov Jacobson <Dov@GamesThatWork.com>
2021-07-09 09:29:59 -0400

    add "once' method

--- signal.js
+++ signal.js
@@ -1,12 +1,17 @@
 export const signal ={
 		bus:	document.querySelector("#bus"),
 		list:	[],
-		fire: 	(event, data   ) => 	signal.bus.dispatchEvent( new CustomEvent( event, { detail:data } ) ),
-		on:   	(event, handler) =>{ 	signal.bus.addEventListener( event, handler );
-										return signal.list.push( {event, handler } );
-										},
-		off:   	i				 =>{ 	let o = signal.list[ i ];
-										signal.bus.removeEventListener( o.event, o,handler );
-										},
-		clear:  ()=>   signal.bus.parentNode.replaceChild( signal.bus.cloneNode(true), signal.bus),
-		}
+
+		fire: 	(event, data         ) 	=> 	signal.bus.dispatchEvent( new CustomEvent( event, { detail:data } ) ),
+
+		on:   	(event, handler, cfg ) 	=>{ signal.bus.addEventListener( event, handler, cfg );
+											return signal.list.push( {event, handler } );
+											},
+		once:   (event, handler) 		=>	signal.on( event, handler, {once:true}),
+
+		off:   	(event,handler)	 		=> 	signal.bus.removeEventListener( event, handler ),
+		
+		remove: i					 	=>	signal.off( signal.list[ i ].event, signal.list[ i ].handler ),
+
+		clear:  ()						=>  signal.bus.replaceWith( signal.bus.cloneNode(true) )
+		};
