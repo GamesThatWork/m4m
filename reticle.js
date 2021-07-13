e827473123b3a2cca16205afad69cf7924a69c25
Dov Jacobson <Dov@GamesThatWork.com>
2021-07-13 16:40:49 -0400

    make reentrant align methods with other features

--- reticle.js
+++ reticle.js
@@ -1,58 +1,95 @@
 import  { signal  } from './signal.js'    ;
 
-export const newReticle = cnfg => {
-    const r=cnfg; 
+    let exists = null;
+
+export const newReticle = cfg => {
+	
+	if( exists )	return exists;
+
+	const r=cfg; 
 	const root  = new PIXI.Graphics();
     const graphicLayers=[new PIXI.Graphics(), new PIXI.Graphics()];
     const lineWidth = [9,3];
-    const circle = [];
+    const circle = [], xhair=[];
     const blurFilter = new PIXI.filters.BlurFilter();
+    const colorHex = [
+			{ red:0xDD0000, yellow:0xAAAA00, blue:0x0044FF, green:0x00DD00, grey:0x008800 },
+			{ red:0xFF4444, yellow:0xFFFF44, blue:0x5588FF, green:0x44FF44, grey:0x228822 },
+	        
+					];
+	var colorOveride;
+
 console.log( signal );
 
     const self={
-      get g(   ) { return root; 						      },
-      get x(   ) { return graphicLayers[0].position.x;        },
-      get y(   ) { return graphicLayers[0].position.y;        },
-      set x( v ) { graphicLayers.forEach(g => g.position.x=v) },
-      set y( v ) { graphicLayers.forEach(g => g.position.y=v) },
-  
-      construct: ()=>{
-		r.min        = r.min        ?? {};
-		r.max        = r.max        ?? {};
-		r.min.color  = r.min.color  ?? 0xFF1100;
-		r.max.color  = r.max.color  ?? 0xDDAA00;
-		r.min.radius = r.min.radius ?? 50;
-		r.max.radius = r.max.radius ?? 50;
-	    graphicLayers.forEach( (g, layer) =>{
-          blurFilter.blur = 10;
-          if( !layer ) g.filters = [blurFilter];    
-          g.lineStyle( lineWidth[ layer ],  [0x00FF00,0x80FF80][layer], 1);
-          g.moveTo( 0,   20);
-          g.lineTo( 0, 2000);
-          g.moveTo( 0,  -20);
-          g.lineTo( 0,-2000);
-          g.moveTo(   20, 0);
-          g.lineTo( 2000, 0);
-          g.moveTo(  -20, 0);
-          g.lineTo(-2000, 0);
-		  self.radius( 50 );
-		  root.addChild( g );
-          }); 
-	  	r.parent.addChild( root );
-		self.show();
-        return self;
-        },
-		
-		circle:[],
+	//	get g(   ) { return root; 						      },
+	//	get x(   ) { return graphicLayers[0].position.x;        },
+	//	get y(   ) { return graphicLayers[0].position.y;        },
+	//	set x( v ) { graphicLayers.forEach(g => g.position.x=v) },
+	//	set y( v ) { graphicLayers.forEach(g => g.position.y=v) },
+		x:  v =>  { graphicLayers.forEach( g => g.position.x=v);	return self; },
+		y:  v =>  { graphicLayers.forEach( g => g.position.y=v);	return self; },
 
-	  	radius: newRadius =>{
+		position: pos => { 
+			graphicLayers.forEach(g => {
+				g.position.x=pos[0];
+				g.position.y=pos[1];  
+				});
+			return self;
+			},
+
+		construct: ()=>{
+			r.min        = r.min        ?? {};
+			r.max        = r.max        ?? {};
+			r.min.color  = r.min.color  ?? 0xFF1100;
+			r.max.color  = r.max.color  ?? 0xDDAA00;
+			r.min.radius = r.min.radius ?? 50;
+			r.max.radius = r.max.radius ?? 50;
 			graphicLayers.forEach( (g, layer) =>{
- 				if( circle[ layer ] )	
+				blurFilter.blur = 10;
+				if( !layer ) g.filters = [blurFilter];    
+				root.addChild( g );
+				}); 
+			self.lines( );
+			self.radius( 50 );
+			r.parent.addChild( root );
+			self.show();
+			return self;
+			},
+		
+	  	radius: (newRadius,color="green") =>{
+			graphicLayers.forEach( (g, layer) =>{
+ 				if( circle[ layer ] )	{
 				 	circle[ layer ].destroy();
-				circle[ layer ] = new PIXI.Graphics()
-									.lineStyle(   lineWidth[ layer ], r.max.color, 1)
-									.drawCircle(  0, 0, r.max.radius = newRadius  );
-				g.addChild( circle[ layer ] );
+					circle[ layer ]= null;
+					 }
+				if( newRadius ){
+					circle[ layer ] = new PIXI.Graphics()
+						.lineStyle(   lineWidth[ layer ], colorHex[layer][color], 1)
+						.drawCircle(  0, 0, r.max.radius = newRadius  );
+					g.addChild( circle[ layer ] );
+					}
+				});
+			return self;
+			},
+
+	  	lines: (color="green") =>{
+			graphicLayers.forEach( (g, layer) =>{
+				if( xhair[ layer ] )	{
+				 	xhair[ layer ].destroy();
+					xhair[ layer ]= null;
+					}
+				xhair[ layer ]= new PIXI.Graphics()
+					.lineStyle( lineWidth[ layer ], colorHex[layer][color], 1)//  [0x00FF00,0x80FF80][layer], 1);
+					.moveTo( 0,   20)
+					.lineTo( 0, 2000)
+					.moveTo( 0,  -20)
+					.lineTo( 0,-2000)
+					.moveTo(   20, 0)
+					.lineTo( 2000, 0)
+					.moveTo(  -20, 0)
+					.lineTo(-2000, 0);
+				g.addChild( xhair[ layer ] );
 				});
 			return self;
 			},
@@ -78,5 +115,5 @@ console.log( signal );
 			return self;
 			}
 		}
-		return self.construct().show();
+		return exists = self.construct().show();
 		}
