73a587e951bbb6bbca8944a0db5e03a1ee7232a4
Dov Jacobson <Dov@GamesThatWork.com>
2021-07-13 16:48:14 -0400

    very clean refactor
    support fullscreen , kill , fadeout

--- pic.js
+++ pic.js
@@ -1,4 +1,5 @@
 
+import  { signal     } from './signal.js'    ;
 
 	const url= { 	
 	imgserver: "https://storage.googleapis.com/storage/v1/b/unscrew-u/" ,
@@ -51,53 +52,59 @@ export const newPic= (name, cfg) => {
 		if( selfs[ name ] )	return selfs[name];
 	console.log("= new Pic", name );
 
-		const dom={
-			root: document.querySelector("#pix")
-			}
-		const div = null;
+		const root= document.querySelector("#pix");
+		const div = document.createElement("div");
+		const style = div.style;
+		const classList = div.classList;
+		div.id= name;
+		classList.add("pic");
+		style.backgroundImage=	`url("${ typeof url[ name ]=="string"?url[ name ] : url[ name ][0] }" )`;
+		root.appendChild( div );
+
+
 		const self ={ 
 			position: (x,y,z=1)=>
-						{ dom[ name ].style.transform=`translate(${x}px,${y}px) scale(${z})`; return self;	  },
-			full:  ()=> { 
-					//Array.from( document.querySelectorAll(".pic.fullscreen")).forEach( p=> newPic(p.id).kill());
-					dom[ name ].classList.add( "fullscreen" );                         
-					return self;    
-					},
-			big:   	()=> { dom[ name ].classList.remove( "small" );                            return self;    },
-			small: 	()=> { dom[ name ].classList.add(    "small" );                            return self;    },
-			show:	()=> { dom[ name ].style.display="block";                                  return self;    },
-			hide:	()=> { dom[ name ].style.display="none"; console.log("Hide ", name );      return self;    },
-			kill:	()=> { dom[ name ].remove(); delete selfs[ name ];						  return self;    },
-			fadeout:()=> { dom[ name ].style.transition= console.log( selfs ); dom[ name ].remove(); delete selfs[ name ];	console.log( selfs );						  return self;    },
+						 { style.transform=`translate(${x}px,${y}px) scale(${z})`; return self;	 },
+			full:  	()=> { classList.add( "fullscreen" );                          return self;  },
+			big:   	()=> { classList.remove( "small" );                            return self;  },
+			small: 	()=> { classList.add(    "small" );                            return self;  },
+			show:	()=> { style.display="block";                                  return self;  },
+			hide:	()=> { style.display="none"; console.log("Hide ", name );      return self;  },
+			kill:	()=> { 	div.remove(); 
+							delete selfs[name]; 
+							signal.fire("pic.kill");						 	   return self;  },
+			fadeout:()=> { 	style.transition="opacity 4s"; 
+							style.opacity="0"; 
+							setTimeout( self.kill, 4000 );                         return self;  },
 			scan:  	()=> {	
 					const zoom =4;
 					if( !self._scan ){   
-						dom[ name ].style.transition="transform 0s";
+						style.transition="transform 0s";
 						self._scan=	( target=> e=>target.position( 
 								Math.min(Math.max(0,e.x*4-2400), 3840/zoom), 
 								Math.min(Math.max(0,e.y*4-1350), 2160/zoom),
 								zoom ))( self );
 						document.body.addEventListener( "mousemove", self._scan );
 						document.body.addEventListener( "click",     self.scan );
-						self._scan( {target:dom[ name ], x:960,y:490});
+						self._scan( {target:div, x:960,y:490});
 						}
 					else{
 						document.body.removeEventListener( "mousemove", self._scan );
-						dom[ name ].style.transition="transform 1s erase-in ease-out 1.5s";
-						dom[ name ].style.transform="translate( 975px,0px) scale(.9) "; 
+						style.transition="transform 1s erase-in ease-out 1.5s";
+						style.transform="translate( 975px,0px) scale(.9) "; 
 						self._scan= null;
 						}},
 		
 		
 			zoom:  ()=> {
-				dom[ name ].style.transform="scale(7) translate(245px,135px)"; 
-				dom[ name ].style.opacity="0"; 
-//				console.log( dom[ name ].style );	
+				style.transform="scale(7) translate(245px,135px)"; 
+				style.opacity="0"; 
+//				console.log(style );	
 				setTimeout( e=>{
-					dom[ name ].style.transition="transform 1s erase-in ease-out .5s, opacity 1s ease-out ";
-					dom[ name ].style.transform="translate( 975px,0px) scale(.9) "; 
-					dom[ name ].style.opacity="1"; 
-//					console.log( dom[ name ].style );	
+					style.transition="transform 1s erase-in ease-out .5s, opacity 1s ease-out ";
+					style.transform="translate( 975px,0px) scale(.9) "; 
+					style.opacity="1"; 
+//					console.log( style );	
 					}, 100);
 				return self;
 				},
@@ -115,8 +122,8 @@ export const newPic= (name, cfg) => {
 						if( self.paused )	return;
 						let t = basis+variation*Math.random();
 						self.timeoutID = setTimeout( next, 1000*t+250 );
-						dom[ name ].style.transition=`background-image ${t}s`;
-						dom[ name ].style.backgroundImage=
+						transition=`background-image ${t}s`;
+						style.backgroundImage=
 						`url("${url[ name ][Math.floor(Math.random()*url[ name ].length)]}"),
 							url("${url.bg}`;
 						}  
@@ -124,24 +131,7 @@ export const newPic= (name, cfg) => {
 				return self;
 				},
 			pause: (pausing=true) => self.rando( !pausing )
-			/*trans: (start, end)=>{
-			
-				const next= ()=>{
-					dom[ name ].style.transition="background-image 2s";
-					dom[ name ].style.backgroundImage=
-						`url("${url[ name ][Math.floor(Math.random()*url[ name ].length)]}"),
-						url("${url.bg}`;
-					setTimeout( next, 750+2250*Math.random() );
-					}  
-				next();
-				return self;
-				}*/
 			}
-		dom[ name ] = 	document.createElement("div");
-		dom[ name ].id= name;
-		dom[ name ].classList.add("pic");
-		dom[ name ].style.backgroundImage=	`url("${ typeof url[ name ]=="string"?url[ name ] : url[ name ][0] }" )`;
-		dom.root.appendChild( dom[ name ] )
 		return selfs[ name ]=self;
 	}
 
