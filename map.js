1db2927873f45e8d4b715c1b55017e75355398da
Dov Jacobson <Dov@GamesThatWork.com>
2021-07-21 23:50:16 -0400

    autohide pre-existing maps, more maps, new names

--- map.js
+++ map.js
@@ -1,26 +1,31 @@
 
 const url= { 	
-	server  :  "./assets/map/",
-	bridges :  "3bridges.png",
-	range   :  "eglinred.jpg",
-	title   :  "title.png",
-	desktop :  "codex.png",         //"desktop(dark).jpg",
-	blur    :  "codeglitch.jpg"// "codeblur.png"       //"desktop(dark).jpg",
+	server   :  "./assets/map/",
+	bridges  :  "bridges_123.png",
+	bridges1 :  "bridges_1.png",
+	bridges23:  "bridges_23.png",
+	range    :  "eglinred.jpg",
+	title    :  "title.png",
+	black    :  "black.jpg",
+	desktop  :  "codex.png",         //"desktop(dark).jpg",
+	blur     :  "codeglitch.jpg"// "codeblur.png"       //"desktop(dark).jpg",
 	};
 const dom ={
 	parent: document.querySelector("#map"), 
 	}
-const selfs=[];
+const selfs={};
  
 
 export const newMap = (name,cfg={}) => {
 
 	console.log("map", name );
-	if( selfs[ name ] )	return selfs[name];
-		
+	if( selfs[ name ] )	return selfs[name].show();
+
+	console.log("map new => ", name );
 	
     const self={
 		get dom() { return dom[ name ] },
+		
 		construct: ()=>{
 			dom[ name ] = document.createElement("div");
 			dom[ name ].id= name;
@@ -40,9 +45,10 @@ export const newMap = (name,cfg={}) => {
 			},
 
 		img:   i =>{	/*unsupported as command*/   return self;},
-		move:  m =>	{ dom[ name ].style.transform=`translate(${m[0]}px,${m[1]}px) scale(${m[2]||1})`; return self;},
-		show:  ()=> { dom[ name ].style.display="block";   console.log("show ", dom[name] );       return self;},
-		hide:  ()=> { dom[ name ].style.display="none";    console.log("Hide ", dom[name] );       return self;},
+		move:  m =>	{ 	dom[ name ].style.transform=`translate(${m[0]}px,${m[1]}px) scale(${m[2]||1})`; return self;},
+		hide:  ()=> {	dom[ name ].style.display="none";       console.log("Hide ", dom[name] );       return self;},
+		_show: ()=> { 	dom[ name ].style.display="block";      console.log("show ", dom[name] );       return self;},
+		show:  ()=> { 	Object.values( selfs ).forEach( s=> s==self? s._show() : s.hide() );                             return self;},
         }
     return selfs[ name ]=self.construct();
     }
